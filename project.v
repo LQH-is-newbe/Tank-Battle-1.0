edcc9120a5b82e498befece000922ae2162254e2
LQH-is-newbe <52833716+LQH-is-newbe@users.noreply.github.com>
2019-08-02 22:24:35 -0400

    Update project.v

--- project.v
+++ project.v
@@ -53,6 +53,8 @@ module project
 	assign start = ~KEY[0];
 	assign mapselect = SW[1:0];
 	
+	//keyboard module modified from https://www.stepfpga.com/doc/ps2%E9%94%AE%E7%9B%98%E6%A8%A1%E5%9D%97. 
+	//This module is initially only capable for at most one key being pressed at a time. We modified it to be capable for multiple keys to adapt our need. Also, we assigned keys to direction and fire signals.
 	Keyboard_PS2(
 	.clk_in(CLOCK_50),				
 	.rst_n_in(resetn),			
